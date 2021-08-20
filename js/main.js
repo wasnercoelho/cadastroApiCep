097f6cb2c1c2249ea9f7b6b712ae3c42e06fa69f
wasnercoelho <@wasnercoelho>
2021-08-19 23:04:27 -0400

    fazendo atualizações

---
+++ js/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const preencherFormulario = (endereco) =>{
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+const pesquisarCep = async() => {
+    const cep = document.getElementById('cep').value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    if (cepValido(cep)){
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        if (endereco.hasOwnProperty('erro')){
+            alert('CEP não encontrado!');
+        }else {
+            preencherFormulario(endereco);
+        }      
+    }else{
+        alert('CEP invalido. tente novamente.');
+    }
+    
+
+}
+
+document.getElementById('cep')
+        .addEventListener('focusout',pesquisarCep);
